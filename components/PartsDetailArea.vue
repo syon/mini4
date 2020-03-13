42e2a4abc67d47a804998514012f4f4cfa0b058b
syon <syon.xiii@gmail.com>
2020-03-14 07:10:20 +0900

    :zap: parts detail

--- components/PartsDetailArea.vue
+++ components/PartsDetailArea.vue
@@ -1,12 +1,51 @@
 <template>
   <div>
     <div class="flex">
-      <div class="w-12 h-12 bg-gray-300"></div>
-      <div class="w-12 h-12 border text-xs">スピード</div>
-      <div class="w-12 h-12 border text-xs">パワー</div>
-      <div class="w-12 h-12 border text-xs">コーナー安定</div>
-      <div class="w-12 h-12 border text-xs">スタミナ耐久</div>
-      <div class="w-12 h-12 border text-xs">重さ</div>
+      <div class="w-16 h-16 bg-gray-300"></div>
+      <div class="flex-auto flex flex-col items-center justify-center text-xs">
+        <div>スピード</div>
+        <div class="text-lg">{{ ingItem.params.速度 }}</div>
+      </div>
+      <div class="flex-auto flex flex-col items-center justify-center text-xs">
+        <div>パワー</div>
+        <div class="text-lg">{{ ingItem.params.馬力 }}</div>
+      </div>
+      <div class="flex-auto flex flex-col items-center justify-center text-xs">
+        <div>コーナー安定</div>
+        <div class="text-lg">{{ ingItem.params.安定 }}</div>
+      </div>
+      <div class="flex-auto flex flex-col items-center justify-center text-xs">
+        <div>スタミナ耐久</div>
+        <div class="text-lg">{{ ingItem.params.耐久 }}</div>
+      </div>
+      <div class="flex-auto flex flex-col items-center justify-center text-xs">
+        <div>重さ</div>
+        <div class="text-lg">{{ ingItem.params.重量 }}g</div>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState('ing', {
+      ingPart: (state) => state.part
+    }),
+    ...mapState('catalog', {
+      dataset: (state) => state.dataset
+    }),
+    ingItem() {
+      const partCatalog = this.dataset[this.ingPart]
+      const partRecipe = this.$store.state.recipe[this.ingPart]
+      const key = partRecipe // TODO
+      return partCatalog[key] || { params: {} }
+    },
+    ingDataset() {
+      return this.dataset[this.ingPart]
+    }
+  }
+}
+</script>
