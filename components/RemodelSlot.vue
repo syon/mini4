93523e2a8765e7e1d4aa9e9ec39ac82642b5413e
syon <syon.xiii@gmail.com>
2020-03-14 12:57:43 +0900

    :art: styling

--- components/RemodelSlot.vue
+++ components/RemodelSlot.vue
@@ -4,21 +4,23 @@
       <div>{{ x.action }}</div>
       <div>詳細</div>
     </div>
-    <div class="flex">
+    <div class="flex h-16">
       <div class="flex w-24">
-        <div class="flex-auto flex flex-col items-center justify-center">
-          <div>{{ x.quarity }}</div>
-          <div>Lv.{{ x.level }}</div>
+        <div
+          class="xx-quarity flex-auto flex flex-col items-center justify-center"
+        >
+          <div class="xx-quarity-label">{{ x.quarity }}</div>
+          <div class="xx-quarity-level">Lv.{{ x.level }}</div>
         </div>
       </div>
 
       <div class="flex-1 flex flex-col">
         <template v-for="(e, idx) in x.effects">
-          <div :key="idx" class="flex">
-            <div class="w-24">メリット</div>
-            <div class="flex-1 flex justify-between">
+          <div :key="idx" :class="{ isMerit: true }" class="xx-merideme flex">
+            <div class="xx-merideme-label w-24">メリット</div>
+            <div class="xx-merideme-topic flex-1 flex justify-between">
               <div>{{ e.影響 }}</div>
-              <div>{{ e.程度 }}</div>
+              <div class="xx-merideme-marks">{{ e.程度 }}</div>
             </div>
           </div>
         </template>
@@ -46,5 +48,38 @@ export default {
   padding: 2px 4px;
   color: #fff;
   background-color: #20272a;
+}
+
+.xx-quarity {
+  .xx-quarity-label {
+    font-size: 1.25rem;
+    font-weight: bold;
+    line-height: 1;
+  }
+  .xx-quarity-level {
+    font-size: 1.25rem;
+    font-weight: bold;
+    line-height: 1.2;
+  }
+}
+
+.xx-merideme {
+  margin: 5px;
+  &.isMerit {
+    .xx-merideme-label {
+      margin-right: 10px;
+      background-color: #5cfe5a;
+      border: 1px solid #28cb25;
+      text-align: center;
+    }
+    .xx-merideme-topic {
+      padding-left: 10px;
+      background-color: #ceeed0;
+    }
+    .xx-merideme-marks {
+      color: #0d7827;
+      font-weight: bold;
+    }
+  }
 }
 </style>
