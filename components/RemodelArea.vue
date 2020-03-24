c6a1aea2592de2032fa44d9959e5051851b443cb
syon <syon.xiii@gmail.com>
2020-03-25 03:17:03 +0900

    :art: remodel slot margin

--- components/RemodelArea.vue
+++ components/RemodelArea.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="xx-RemodelArea flex flex-col">
     <template v-for="(x, idx) in ingCrafts">
-      <div :key="idx" class="flex mb-1">
+      <div :key="idx" class="flex mb-2">
         <remodel-slot :arg="x" :craft-index="idx" />
       </div>
     </template>
