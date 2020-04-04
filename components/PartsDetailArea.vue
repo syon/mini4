0f7ef66ad2987ac6ac8f263f2abd13b3170317d6
syon <syon.xiii@gmail.com>
2020-04-04 22:33:31 +0900

    :art: BodyFeature

--- components/PartsDetailArea.vue
+++ components/PartsDetailArea.vue
@@ -8,8 +8,23 @@
           <td>{{ ingItem.コース適性 }}</td>
         </tr>
         <tr v-if="ingItem.ボディ特性">
-          <th>ボディ特性</th>
-          <td>{{ ingItem.ボディ特性 }}</td>
+          <th colspan="2">
+            <div class="BodyFeature mb-1">
+              <div class="BodyFeature-Floor">
+                <div class="BodyFeature-Content">
+                  <div>
+                    <span class="BodyFeature-Label">ボディ特性</span>
+                  </div>
+                  <hr class="BodyFeature-Hr" />
+                  <div class="text-right">
+                    <span class="BodyFeature-Text">{{
+                      ingItem.ボディ特性
+                    }}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </th>
         </tr>
       </tbody>
     </table>
@@ -125,6 +140,41 @@ export default {
   }
   td {
     text-align: right;
+  }
+}
+
+.BodyFeature {
+  background-color: rgb(2, 4, 5);
+  border: 1px solid #73e6ea;
+  border-radius: 4px;
+
+  .BodyFeature-Floor {
+    background: radial-gradient(
+      circle,
+      rgba(2, 4, 5, 1) 0%,
+      rgba(55, 93, 116, 1) 75%
+    );
+    border-radius: 3px;
+  }
+  .BodyFeature-Content {
+    display: flex;
+    flex-direction: column;
+    padding: 0.4em;
+  }
+  .BodyFeature-Label {
+    font-size: 0.6rem;
+    background-color: #5fcffa;
+    color: black;
+    padding: 0 0.5em;
+    border-radius: 4px;
+  }
+  .BodyFeature-Hr {
+    margin: 0.2em 0;
+    border: 0;
+    border-bottom: 1px solid #59a6f2;
+  }
+  .BodyFeature-Text {
+    flex: 1;
   }
 }
 </style>
