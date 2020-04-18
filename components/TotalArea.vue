dacc5e8a2b032edbfa80a4b5259b64b5cd79bcf5
syon <syon.xiii@gmail.com>
2020-04-19 01:06:33 +0900

    :zap: TotalDetail

--- components/TotalArea.vue
+++ components/TotalArea.vue
@@ -77,59 +77,8 @@
         </table>
       </div>
     </div>
-    <div
-      v-if="isDetailOpen"
-      class="TotalArea-Detail py-2 px-4 zzBg-gray2 text-xs"
-    >
-      <div class="flex items-center justify-between mb-2 mx-2">
-        <div class="flex">
-          <div v-for="a in aptSet" :key="a" class="flex mr-2">
-            <apti-hex :type="a" />
-            <span class="zzAnton pl-1">{{ totalAptitude[a] }}</span>
-          </div>
-        </div>
-        <div class="BodyFeature">
-          <div class="BodyFeature-Floor">
-            <div class="BodyFeature-Content">
-              <div class="BodyFeature-Label">ボディ<br />特性</div>
-              <div class="BodyFeature-Text">{{ bodyInfo.ボディ特性 }}</div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="flex">
-        <div class="flex-1 px-2">
-          <score-cell label="スピード" :score="scores.スピード" />
-          <score-cell label="パワー" :score="scores.パワー" />
-          <score-cell label="コーナー安定" :score="scores.コーナー安定" />
-          <score-cell label="スタミナ耐久" :score="scores.スタミナ耐久" />
-          <score-cell label="重さ" :score="scores.重さ" />
-          <score-cell label="ギヤ負荷" :score="scores.ギヤ負荷" />
-          <score-cell label="パワーロス" :score="scores.パワーロス" />
-          <score-cell label="スピードロス" :score="scores.スピードロス" />
-          <score-cell
-            label="エアロダウンフォース"
-            :score="scores.エアロダウンフォース"
-          />
-          <score-cell label="節電" :score="scores.節電" />
-          <score-cell label="制振" :score="scores.制振" />
-          <score-cell label="スラスト角" :score="scores.スラスト角" />
-        </div>
-        <div class="flex-1 px-2">
-          <score-cell label="タイヤ摩擦" :score="scores.タイヤ摩擦" />
-          <score-cell label="タイヤ旋回" :score="scores.タイヤ旋回" />
-          <score-cell label="タイヤ反発" :score="scores.タイヤ反発" />
-          <score-cell label="タイヤ径" :score="scores.タイヤ径" />
-          <score-cell label="ローラー摩擦" :score="scores.ローラー摩擦" />
-          <score-cell label="ローラー抵抗" :score="scores.ローラー抵抗" />
-          <score-cell label="ウェーブ" :score="scores.ウェーブ" />
-          <score-cell label="オフロード" :score="scores.オフロード" />
-          <score-cell label="ギヤ比" :score="scores.ギヤ比" />
-          <score-cell label="消費電流" :score="scores.消費電流" />
-          <score-cell label="ブレーキ減速" :score="scores.ブレーキ減速" />
-          <score-cell label="スタビ減速" :score="scores.スタビ減速" />
-        </div>
-      </div>
+    <div v-if="isDetailOpen" class="py-2 px-4 zzBg-gray2 text-xs">
+      <total-detail />
     </div>
     <button class="w-full text-center border" @click="toggleDetail">
       <span v-if="isDetailOpen">▲</span>
@@ -139,16 +88,14 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
+import { mapGetters } from 'vuex'
 import ScoreRank from '@/components/ScoreRank'
-import ScoreCell from '@/components/ScoreCell'
-import AptiHex from '@/components/AptiHex'
+import TotalDetail from '@/components/TotalDetail'
 
 export default {
   components: {
     ScoreRank,
-    ScoreCell,
-    AptiHex,
+    TotalDetail,
   },
   data() {
     return {
@@ -156,22 +103,9 @@ export default {
     }
   },
   computed: {
-    ...mapState('ing', {
-      ingPartRecipe: (state) => state.partRecipe,
-      ingItem: (state) => state.item,
-      ingCrafts: (state) => state.crafts,
-    }),
-    ...mapState('craft', {
-      craftMaster: (state) => state.craft,
-    }),
     ...mapGetters({
-      gAllEquips: 'recipe/gAllEquips',
-      getEquipByPart: 'recipe/getEquipByPart',
       scores: 'recipe/gAllPartScoresSum',
     }),
-    bodyInfo() {
-      return this.getEquipByPart('ボディ')
-    },
     totalScore() {
       let score = 0
       const basics = [
@@ -187,19 +121,6 @@ export default {
         }
       }
       return score
-    },
-    aptSet() {
-      return ['St', 'U', 'Co', 'Kp', 'J']
-    },
-    totalAptitude() {
-      const apts = this.gAllEquips
-        .map((x) => x.item.コース適性)
-        .filter((x) => this.aptSet.includes(x))
-      const result = {}
-      for (const a of apts) {
-        result[a] = (result[a] || 0) + 1
-      }
-      return result
     },
   },
   methods: {
@@ -269,12 +190,6 @@ export default {
     table {
       table-layout: fixed;
     }
-  }
-}
-
-.TotalArea-Detail {
-  .isZero {
-    color: #888;
   }
 }
 
