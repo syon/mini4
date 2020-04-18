9a584009f23f61684e128f85733c6cdb2837e414
syon <syon.xiii@gmail.com>
2020-04-19 04:16:52 +0900

    :bar_chart: access counter

---
+++ components/AccessCounter.vue
@@ -0,0 +1,6 @@
+<template
+  ><img
+    src="https://www.f-counter.net/j/33/1587237145/"
+    alt="アクセスカウンター"
+  />
+</template>
--- components/FooterArea.vue
+++ components/FooterArea.vue
@@ -1,18 +1,26 @@
 <template>
   <div>
     <about-modal />
-    <div class="flex items-center justify-center p-4">
-      <button class="zzBtn1" @click="handleAbout">About</button>
+    <div class="flex items-center justify-between p-4">
+      <div class="flex-1"></div>
+      <div class="flex-1 text-center">
+        <button class="flex-1 zzBtn1" @click="handleAbout">About</button>
+      </div>
+      <div class="flex-1">
+        <access-counter class="flex-1 ml-auto" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import AboutModal from '@/components/AboutModal'
+import AccessCounter from '@/components/AccessCounter'
 
 export default {
   components: {
     AboutModal,
+    AccessCounter,
   },
   methods: {
     handleAbout() {
