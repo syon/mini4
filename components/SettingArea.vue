0b9d173714e0a5c0f6b3dba70ed289d493204042
syon <syon.xiii@gmail.com>
2020-05-06 16:01:23 +0900

    :zap: Garage grid

--- components/SettingArea.vue
+++ components/SettingArea.vue
@@ -1,190 +1,275 @@
 <template>
   <div class="xx-SettingArea">
-    <div class="grid-setting">
-      <wear-box
-        :item="sBody"
-        part="ボディ"
-        class="cate-body"
-        @click="handleTrans('ボディ')"
-      />
-      <wear-box
-        :item="sMotor"
-        part="モーター"
-        class="cate-motor"
-        @click="handleTrans('モーター')"
-      />
-      <wear-box
-        :item="sGear"
-        part="ギヤ"
-        class="cate-gear"
-        @click="handleTrans('ギヤ')"
-      />
-
-      <wear-box
-        :item="sChassis"
-        part="シャーシ"
-        class="cate-chassis"
-        @click="handleTrans('シャーシ')"
-      />
-
-      <wear-box
-        :item="sFrontWheel"
-        part="フロント・ホイール"
-        class="cate-front-wheel"
-        @click="handleTrans('フロント・ホイール')"
-      />
-      <wear-box
-        :item="sFrontTire"
-        part="フロント・タイヤ"
-        class="cate-front-tire"
-        @click="handleTrans('フロント・タイヤ')"
-      />
-      <wear-box
-        :item="sRearWheel"
-        part="リヤ・ホイール"
-        class="cate-rear-wheel"
-        @click="handleTrans('リヤ・ホイール')"
-      />
-      <wear-box
-        :item="sRearTire"
-        part="リヤ・タイヤ"
-        class="cate-rear-tire"
-        @click="handleTrans('リヤ・タイヤ')"
-      />
-
-      <wear-box
-        :item="sFrontStay"
-        part="フロントステー"
-        class="cate-front-stay"
-        @click="handleTrans('フロントステー')"
-      />
-      <wear-box
-        :item="sSideStay"
-        part="サイドステー"
-        class="cate-side-stay"
-        @click="handleTrans('サイドステー')"
-      />
-      <wear-box
-        :item="sRearStay"
-        part="リヤステー"
-        class="cate-rear-stay"
-        @click="handleTrans('リヤステー')"
-      />
-
-      <wear-box
-        :item="sFrontRollerHigh"
-        part="フロント・ローラー上"
-        class="cate-front-roller-high"
-        @click="handleTrans('フロント・ローラー上')"
-      />
-      <wear-box
-        :item="sSideRollerHigh"
-        part="サイド・ローラー上"
-        class="cate-side-roller-high"
-        @click="handleTrans('サイド・ローラー上')"
-      />
-      <wear-box
-        :item="sRearRollerHigh"
-        part="リヤ・ローラー上"
-        class="cate-rear-roller-high"
-        @click="handleTrans('リヤ・ローラー上')"
-      />
-
-      <wear-box
-        :item="sFrontRollerMiddle"
-        part="フロント・ローラー中"
-        class="cate-front-roller-middle"
-        @click="handleTrans('フロント・ローラー中')"
-      />
-      <wear-box
-        :item="sSideRollerMiddle"
-        part="サイド・ローラー中"
-        class="cate-side-roller-middle"
-        @click="handleTrans('サイド・ローラー中')"
-      />
-      <wear-box
-        :item="sRearRollerMiddle"
-        part="リヤ・ローラー中"
-        class="cate-rear-roller-middle"
-        @click="handleTrans('リヤ・ローラー中')"
-      />
-
-      <wear-box
-        :item="sRearRollerLow"
-        part="リヤ・ローラー下"
-        class="cate-rear-roller-low"
-        @click="handleTrans('リヤ・ローラー下')"
-      />
-
-      <wear-box
-        :item="sBodyOption"
-        part="ボディオプション"
-        class="cate-bodyoption"
-        @click="handleTrans('ボディオプション')"
-      />
-      <wear-box
-        :item="sWingRoller"
-        part="ウイングローラー"
-        class="cate-wing-roller"
-        @click="handleTrans('ウイングローラー')"
-      />
-
-      <wear-box
-        :item="sFrontStabilizer"
-        part="フロント・スタビライザー"
-        class="cate-front-stabilizer"
-        @click="handleTrans('フロント・スタビライザー')"
-      />
-      <wear-box
-        :item="sSideStabilizer"
-        part="サイド・スタビライザー"
-        class="cate-side-stabilizer"
-        @click="handleTrans('サイド・スタビライザー')"
-      />
-      <wear-box
-        :item="sRearStabilizer"
-        part="リヤ・スタビライザー"
-        class="cate-rear-stabilizer"
-        @click="handleTrans('リヤ・スタビライザー')"
-      />
-
-      <wear-box
-        :item="sFrontSettingWeight"
-        part="フロント・セッティングウェイト"
-        class="cate-front-settingweight"
-        @click="handleTrans('フロント・セッティングウェイト')"
-      />
-      <wear-box
-        :item="sRearSettingWeight"
-        part="リヤ・セッティングウェイト"
-        class="cate-rear-settingweight"
-        @click="handleTrans('リヤ・セッティングウェイト')"
-      />
-
-      <wear-box
-        :item="sAccessory1"
-        part="アクセサリー・１"
-        class="cate-accessory-1"
-        @click="handleTrans('アクセサリー・１')"
-      />
-      <wear-box
-        :item="sAccessory2"
-        part="アクセサリー・２"
-        class="cate-accessory-2"
-        @click="handleTrans('アクセサリー・２')"
-      />
-      <wear-box
-        :item="sAccessory3"
-        part="アクセサリー・３"
-        class="cate-accessory-3"
-        @click="handleTrans('アクセサリー・３')"
-      />
-      <wear-box
-        :item="sAccessory4"
-        part="アクセサリー・４"
-        class="cate-accessory-4"
-        @click="handleTrans('アクセサリー・４')"
-      />
+    <div class="garage">
+      <div class="garage--row">
+        <div class="garage--cell"></div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sBody"
+            part="ボディ"
+            class="cate-body"
+            @click="handleTrans('ボディ')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sMotor"
+            part="モーター"
+            class="cate-motor"
+            @click="handleTrans('モーター')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sGear"
+            part="ギヤ"
+            class="cate-gear"
+            @click="handleTrans('ギヤ')"
+          />
+        </div>
+      </div>
+      <div class="garage--row">
+        <div class="garage--cell"></div>
+        <div class="garage--cell"></div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sChassis"
+            part="シャーシ"
+            class="cate-chassis"
+            @click="handleTrans('シャーシ')"
+          />
+        </div>
+        <div class="garage--cell"></div>
+      </div>
+      <div class="garage--row">
+        <div class="garage--cell">
+          <wear-box
+            :item="sFrontWheel"
+            part="フロント・ホイール"
+            class="cate-front-wheel"
+            @click="handleTrans('フロント・ホイール')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sFrontTire"
+            part="フロント・タイヤ"
+            class="cate-front-tire"
+            @click="handleTrans('フロント・タイヤ')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sRearWheel"
+            part="リヤ・ホイール"
+            class="cate-rear-wheel"
+            @click="handleTrans('リヤ・ホイール')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sRearTire"
+            part="リヤ・タイヤ"
+            class="cate-rear-tire"
+            @click="handleTrans('リヤ・タイヤ')"
+          />
+        </div>
+      </div>
+      <div class="garage--row">
+        <div class="garage--cell"></div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sFrontStay"
+            part="フロントステー"
+            class="cate-front-stay"
+            @click="handleTrans('フロントステー')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sSideStay"
+            part="サイドステー"
+            class="cate-side-stay"
+            @click="handleTrans('サイドステー')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sRearStay"
+            part="リヤステー"
+            class="cate-rear-stay"
+            @click="handleTrans('リヤステー')"
+          />
+        </div>
+      </div>
+      <div class="garage--row">
+        <div class="garage--cell"></div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sFrontRollerHigh"
+            part="フロント・ローラー上"
+            class="cate-front-roller-high"
+            @click="handleTrans('フロント・ローラー上')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sSideRollerHigh"
+            part="サイド・ローラー上"
+            class="cate-side-roller-high"
+            @click="handleTrans('サイド・ローラー上')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sRearRollerHigh"
+            part="リヤ・ローラー上"
+            class="cate-rear-roller-high"
+            @click="handleTrans('リヤ・ローラー上')"
+          />
+        </div>
+      </div>
+      <div class="garage--row">
+        <div class="garage--cell"></div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sFrontRollerMiddle"
+            part="フロント・ローラー中"
+            class="cate-front-roller-middle"
+            @click="handleTrans('フロント・ローラー中')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sSideRollerMiddle"
+            part="サイド・ローラー中"
+            class="cate-side-roller-middle"
+            @click="handleTrans('サイド・ローラー中')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sRearRollerMiddle"
+            part="リヤ・ローラー中"
+            class="cate-rear-roller-middle"
+            @click="handleTrans('リヤ・ローラー中')"
+          />
+        </div>
+      </div>
+      <div class="garage--row">
+        <div class="garage--cell"></div>
+        <div class="garage--cell"></div>
+        <div class="garage--cell"></div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sRearRollerLow"
+            part="リヤ・ローラー下"
+            class="cate-rear-roller-low"
+            @click="handleTrans('リヤ・ローラー下')"
+          />
+        </div>
+      </div>
+      <div class="garage--row">
+        <div class="garage--cell"></div>
+        <div class="garage--cell"></div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sBodyOption"
+            part="ボディオプション"
+            class="cate-bodyoption"
+            @click="handleTrans('ボディオプション')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sWingRoller"
+            part="ウイングローラー"
+            class="cate-wing-roller"
+            @click="handleTrans('ウイングローラー')"
+          />
+        </div>
+      </div>
+      <div class="garage--row">
+        <div class="garage--cell"></div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sFrontStabilizer"
+            part="フロント・スタビライザー"
+            class="cate-front-stabilizer"
+            @click="handleTrans('フロント・スタビライザー')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sSideStabilizer"
+            part="サイド・スタビライザー"
+            class="cate-side-stabilizer"
+            @click="handleTrans('サイド・スタビライザー')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sRearStabilizer"
+            part="リヤ・スタビライザー"
+            class="cate-rear-stabilizer"
+            @click="handleTrans('リヤ・スタビライザー')"
+          />
+        </div>
+      </div>
+      <div class="garage--row">
+        <div class="garage--cell"></div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sFrontSettingWeight"
+            part="フロント・セッティングウェイト"
+            class="cate-front-settingweight"
+            @click="handleTrans('フロント・セッティングウェイト')"
+          />
+        </div>
+        <div class="garage--cell"></div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sRearSettingWeight"
+            part="リヤ・セッティングウェイト"
+            class="cate-rear-settingweight"
+            @click="handleTrans('リヤ・セッティングウェイト')"
+          />
+        </div>
+      </div>
+      <div class="garage--row">
+        <div class="garage--cell">
+          <wear-box
+            :item="sAccessory1"
+            part="アクセサリー・１"
+            class="cate-accessory-1"
+            @click="handleTrans('アクセサリー・１')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sAccessory2"
+            part="アクセサリー・２"
+            class="cate-accessory-2"
+            @click="handleTrans('アクセサリー・２')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sAccessory3"
+            part="アクセサリー・３"
+            class="cate-accessory-3"
+            @click="handleTrans('アクセサリー・３')"
+          />
+        </div>
+        <div class="garage--cell">
+          <wear-box
+            :item="sAccessory4"
+            part="アクセサリー・４"
+            class="cate-accessory-4"
+            @click="handleTrans('アクセサリー・４')"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -247,132 +332,16 @@ export default {
 
 <style lang="less" scoped>
 .xx-SettingArea {
-  padding: 0.25rem;
-
-  .grid-setting {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-    grid-column-gap: 0.25rem;
-    grid-row-gap: 0.25rem;
-    justify-items: stretch;
-    align-items: stretch;
-
-    .cate-body {
-      grid-row: ~'1/2';
-      grid-column: ~'2/3';
+  .garage {
+    .garage--row {
+      display: flex;
+      justify-content: space-between;
+      padding: 0 0.25rem;
+      margin-bottom: 0.25rem;
     }
-    .cate-motor {
-      grid-row: ~'1/2';
-      grid-column: ~'3/4';
-    }
-    .cate-gear {
-      grid-row: ~'1/2';
-      grid-column: ~'4/5';
-    }
-    .cate-chassis {
-      grid-row: ~'2/3';
-      grid-column: ~'3/4';
-    }
-    .cate-front-wheel {
-      grid-row: ~'3/4';
-      grid-column: ~'1/2';
-    }
-    .cate-front-tire {
-      grid-row: ~'3/4';
-      grid-column: ~'2/3';
-    }
-    .cate-rear-wheel {
-      grid-row: ~'3/4';
-      grid-column: ~'3/4';
-    }
-    .cate-rear-tire {
-      grid-row: ~'3/4';
-      grid-column: ~'4/5';
-    }
-    .cate-front-stay {
-      grid-row: ~'4/5';
-      grid-column: ~'2/3';
-    }
-    .cate-side-stay {
-      grid-row: ~'4/5';
-      grid-column: ~'3/4';
-    }
-    .cate-rear-stay {
-      grid-row: ~'4/5';
-      grid-column: ~'4/5';
-    }
-    .cate-front-roller-high {
-      grid-row: ~'5/6';
-      grid-column: ~'2/3';
-    }
-    .cate-side-roller-high {
-      grid-row: ~'5/6';
-      grid-column: ~'3/4';
-    }
-    .cate-rear-roller-high {
-      grid-row: ~'5/6';
-      grid-column: ~'4/5';
-    }
-    .cate-front-roller-middle {
-      grid-row: ~'6/7';
-      grid-column: ~'2/3';
-    }
-    .cate-side-roller-middle {
-      grid-row: ~'6/7';
-      grid-column: ~'3/4';
-    }
-    .cate-rear-roller-middle {
-      grid-row: ~'6/7';
-      grid-column: ~'4/5';
-    }
-    .cate-rear-roller-low {
-      grid-row: ~'7/8';
-      grid-column: ~'4/5';
-    }
-    .cate-bodyoption {
-      grid-row: ~'8/9';
-      grid-column: ~'3/4';
-    }
-    .cate-wing-roller {
-      grid-row: ~'8/9';
-      grid-column: ~'4/5';
-    }
-    .cate-rear-stabilizer {
-      grid-row: ~'9/10';
-      grid-column: ~'2/3';
-    }
-    .cate-side-stabilizer {
-      grid-row: ~'9/10';
-      grid-column: ~'3/4';
-    }
-    .cate-front-stabilizer {
-      grid-row: ~'9/10';
-      grid-column: ~'4/5';
-    }
-    .cate-front-settingweight {
-      grid-row: ~'10/11';
-      grid-column: ~'2/3';
-    }
-    .cate-rear-settingweight {
-      grid-row: ~'10/11';
-      grid-column: ~'4/5';
-    }
-    .cate-accessory-1 {
-      grid-row: ~'11/12';
-      grid-column: ~'1/2';
-    }
-    .cate-accessory-2 {
-      grid-row: ~'11/12';
-      grid-column: ~'2/3';
-    }
-    .cate-accessory-3 {
-      grid-row: ~'11/12';
-      grid-column: ~'3/4';
-    }
-    .cate-accessory-4 {
-      grid-row: ~'11/12';
-      grid-column: ~'4/5';
+    .garage--cell {
+      width: 13.4vw;
+      height: 13.4vw;
     }
   }
 }
