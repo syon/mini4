b7fc939f23786a138b6143e5ca1f99b6f0deb12c
tarolnetg <tarolnetg@gmail.com>
2020-05-25 22:35:57 +0900

    :art: display switch button

--- components/SettingArea.vue
+++ components/SettingArea.vue
@@ -2,10 +2,10 @@
   <div class="xx-SettingArea">
     <div class="garage">
       <div class="garage--row">
-        <div class="garage--cell">
-          <!-- <button class="zzBtnRect1 zz-text06 z-10 leading-tight">
+        <div class="garage--cell items-center flex">
+          <button class="zzBtnRect1 zz-text06 z-10 leading-tight">
             表示<br />切り替え
-          </button> -->
+          </button>
         </div>
         <div class="garage--cell">
           <wear-box
