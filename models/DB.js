1ec8b4ce06466db70e0d4889ed7a3f0e6a120baa
syon <syon.xiii@gmail.com>
2020-06-08 01:13:15 +0900

    :zap: Firestore

---
+++ models/DB.js
@@ -0,0 +1,12 @@
+import FirebaseClient from '@/models/FirebaseClient'
+
+const db = FirebaseClient.firestore
+
+export default class DB {
+  static async getDoc(path) {
+    const doc = await db.doc(path).get()
+    if (doc.exists) {
+      return doc.data()
+    }
+  }
+}
