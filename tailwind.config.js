5d1474017b97e42f7685a8af988ae4ad12f7a3b5
syon <syon.xiii@gmail.com>
2020-06-10 21:01:03 +0900

    :wrench: supress warning in production build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
   theme: {},
   variants: {},
-  plugins: []
+  plugins: [],
+  // https://github.com/nuxt-community/tailwindcss-module/issues/111
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+  },
 }
