60266d8d79f1a063e7709fb4ca7ae5f212ff89b3
syon <syon.xiii@gmail.com>
2020-06-10 21:42:36 +0900

    :recycle: resume Firestore

--- models/DB.js
+++ models/DB.js
@@ -1,12 +1,12 @@
-// import FirebaseClient from '@/models/FirebaseClient'
+import FirebaseClient from '@/models/FirebaseClient'
 
-// const db = FirebaseClient.firestore
+const db = FirebaseClient.firestore
 
 export default class DB {
   static async getDoc(path) {
-    // const doc = await db.doc(path).get()
-    // if (doc.exists) {
-    //   return doc.data()
-    // }
+    const doc = await db.doc(path).get()
+    if (doc.exists) {
+      return doc.data()
+    }
   }
 }
