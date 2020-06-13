21ebfa7aa23fb77e0e9c007c5a933344abcd97ca
syon <syon.xiii@gmail.com>
2020-06-13 16:01:52 +0900

    :zap: Footer layout

--- components/FooterArea.vue
+++ components/FooterArea.vue
@@ -6,22 +6,24 @@
       <div class="p-4">
         <div class="flex justify-between">
           <div class="leftNavs">
-            <button class="zzBtnRounded1" @click="handleSoftReset">
-              リロード
-            </button>
-            <button class="zzBtnRounded1" @click="handleReset">リセット</button>
-            <button class="zzBtnRounded1" @click="handleAbout">
+            <button class="" @click="handleAbout">
               このサイトについて
             </button>
-            <button class="zzBtnRounded1" @click="handleSupport">
+            <button class="" @click="handleSupport">
               支援する
             </button>
             <!-- <button class="zzBtnRounded1" @click="hendleError">
               Error
             </button> -->
           </div>
-          <div class="rightNavs">
-            <access-counter class="flex-1 m-2" />
+          <div class="rightNavs flex flex-col">
+            <button class="zzBtnRounded1" @click="handleSoftReset">
+              リロード
+            </button>
+            <button class="zzBtnRounded1" @click="handleReset">リセット</button>
+            <div>
+              <access-counter class="flex-1 m-2" />
+            </div>
           </div>
         </div>
         <user-area />
@@ -74,13 +76,32 @@ export default {
 <style lang="less" scoped>
 .FooterArea {
   .leftNavs {
+    flex: 1;
+    margin: 0 2rem 0 0;
     > button {
-      margin-bottom: 1rem;
+      display: block;
+      width: 100%;
+      background: linear-gradient(
+        0deg,
+        #252525 0%,
+        #141717 35%,
+        #000000 70%,
+        #282928 100%
+      );
+      border: 1px solid #0e1111;
+      // box-shadow: inset 1px 1px 2px -0.4px #e5e4e5;
+      // border-radius: 9999px;
+      border-radius: 0.25rem;
+      padding: 0.25rem;
+      margin: 0.5rem 0;
     }
   }
   .rightNavs {
     display: flex;
     align-items: flex-end;
+    > button {
+      margin: 0.5rem 0;
+    }
   }
 }
 </style>
